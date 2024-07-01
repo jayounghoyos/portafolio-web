b5c0affdce10db934bc6495632ace984419da941
jayounghoyos <juanandresyounghoyos@gmail.com>
2024-07-01 15:27:25 -0500

    images

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -65,8 +65,8 @@ const HeroSection = () => {
         >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/public/images/hero-imgage.png"
-              alt="hero_image"
+              src="/public/images/hero-image.png"
+              alt="hero-image_of_me"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
               height={300}
